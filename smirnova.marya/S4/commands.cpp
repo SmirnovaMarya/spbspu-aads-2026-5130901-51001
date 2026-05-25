9e2761bd07025ab6b561eba7941f21e94944f4fc
SmirnovaMarya <unlenmaria@gmail.com>
2026-05-26 01:48:09 +0300

    fix CG

--- smirnova.marya/S4/commands.cpp
+++ smirnova.marya/S4/commands.cpp
@@ -3,11 +3,9 @@
 #include <iostream>
 #include <stdexcept>
 
-#include "tree-iter.hpp"
-
 namespace smirnova
 {
-	template< class Predicate >
+  template< class Predicate >
   Dict buildDict(const Dict& source, Predicate predicate)
   {
     Dict result;
@@ -23,121 +21,152 @@ namespace smirnova
     return result;
   }
 
-	template< class Dictionary >
+  template< class Dictionary >
   struct NotContainsPredicate
   {
     const Dictionary& other;
 
-		bool operator()(const typename Dictionary::key_type& key) const
+    bool operator()(const typename Dictionary::key_type& key) const
     {
       return !other.contains(key);
     }
   };
 
-	template< class Dictionary >
+  template< class Dictionary >
   struct ContainsPredicate
   {
     const Dictionary& other;
 
-		bool operator()(const typename Dictionary::key_type& key) const
+    bool operator()(const typename Dictionary::key_type& key) const
     {
       return other.contains(key);
     }
   };
 
-	void printDataset(const DatasetTree& datasets, const std::string& name)
-	{
-		const Dict& dict = datasets.get(name);
+  void printDataset(const DatasetTree& datasets, const std::string& name)
+  {
+    const Dict& dict = datasets.get(name);
 
-		if (dict.empty())
-		{
-			std::cout << "<EMPTY>\n";
-			return;
-		}
+    if (dict.empty())
+    {
+      std::cout << "<EMPTY>\n";
+      return;
+    }
 
-		std::cout << name;
-		for (auto it = dict.cbegin(); it != dict.cend(); ++it)
-		{
-			std::cout << ' ' << it->first << ' ' << it->second;
-		}
-		std::cout << '\n';
-	}
+    std::cout << name;
 
-	Dict complement(const Dict& left, const Dict& right)
-	{
-		return buildDict(left, NotContainsPredicate<Dict>{right});
-	}
+    for (auto it = dict.cbegin(); it != dict.cend(); ++it)
+    {
+      std::cout << ' ' << it->first << ' ' << it->second;
+    }
 
-	Dict intersect(const Dict& left, const Dict& right)
-	{
-		return buildDict(left, ContainsPredicate<Dict>{right});
-	}
+    std::cout << '\n';
+  }
 
-	Dict unionDict(const Dict& left, const Dict& right)
-	{
-		Dict result = left;
+  Dict complement(const Dict& left, const Dict& right)
+  {
+    return buildDict(left, NotContainsPredicate< Dict >{ right });
+  }
 
-		for (auto it = right.cbegin(); it != right.cend(); ++it)
-		{
-			if (!result.contains(it->first))
-			{
-				result.push(it->first, it->second);
-			}
-		}
+  Dict intersect(const Dict& left, const Dict& right)
+  {
+    return buildDict(left, ContainsPredicate< Dict >{ right });
+  }
 
-		return result;
-	}
+  Dict unionDict(const Dict& left, const Dict& right)
+  {
+    Dict result = left;
 
-	void requireNoExtra(std::istringstream& iss)
-	{
-		std::string extra;
+    for (auto it = right.cbegin(); it != right.cend(); ++it)
+    {
+      if (!result.contains(it->first))
+      {
+        result.push(it->first, it->second);
+      }
+    }
 
-		if (iss >> extra)
-			throw std::runtime_error("unexpected token");
-	}
+    return result;
+  }
 
-	void executeCommand(DatasetTree& datasets, std::istringstream& input)
-	{
-		std::istringstream iss(input.str());
+  void requireNoExtra(std::istringstream& iss)
+  {
+    std::string extra;
 
-		std::string command;
-		if (!(iss >> command))
-			throw std::runtime_error("empty command");
+    if (iss >> extra)
+    {
+      throw std::runtime_error("unexpected token");
+    }
+  }
 
-		if (command == "print")
-		{
-			std::string datasetName;
-			if (!(iss >> datasetName))
-				throw std::runtime_error("bad print command");
-			requireNoExtra(iss);
-			printDataset(datasets, datasetName);
-			return;
-		}
+  void executeCommand(DatasetTree& datasets, std::istringstream& input)
+  {
+    std::string command;
 
-		if (command == "complement" || command == "intersect" || command == "union")
-		{
-			std::string newDatasetName;
-			std::string leftName;
-			std::string rightName;
+    if (!(input >> command))
+    {
+      throw std::runtime_error("empty command");
+    }
 
-			if (!(iss >> newDatasetName >> leftName >> rightName))
-				throw std::runtime_error("bad set operation");
-			requireNoExtra(iss);
+    if (command == "print")
+    {
+      std::string datasetName;
 
-			const Dict& left = datasets.get(leftName);
-			const Dict& right = datasets.get(rightName);
+      if (!(input >> datasetName))
+      {
+        throw std::runtime_error("bad print command");
+      }
 
-			if (command == "complement")
-				datasets.push(newDatasetName, complement(left, right));
-			else if (command == "intersect")
-				datasets.push(newDatasetName, intersect(left, right));
-			else
-				datasets.push(newDatasetName, unionDict(left, right));
+      requireNoExtra(input);
 
-			return;
-		}
+      printDataset(datasets, datasetName);
 
-		throw std::runtime_error("unknown command");
-	}
+      return;
+    }
+
+    if (command == "complement" ||
+        command == "intersect" ||
+        command == "union")
+    {
+      std::string newDatasetName;
+      std::string leftName;
+      std::string rightName;
+
+      if (!(input >> newDatasetName >> leftName >> rightName))
+      {
+        throw std::runtime_error("bad set operation");
+      }
+
+      requireNoExtra(input);
+
+      const Dict& left = datasets.get(leftName);
+      const Dict& right = datasets.get(rightName);
+
+      if (command == "complement")
+      {
+        datasets.push(
+          newDatasetName,
+          complement(left, right)
+        );
+      }
+      else if (command == "intersect")
+      {
+        datasets.push(
+          newDatasetName,
+          intersect(left, right)
+        );
+      }
+      else
+      {
+        datasets.push(
+          newDatasetName,
+          unionDict(left, right)
+        );
+      }
+
+      return;
+    }
+
+    throw std::runtime_error("unknown command");
+  }
 }
 
