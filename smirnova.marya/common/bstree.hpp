c5e9ab82426fc78b153a59f5088f7f039ff5f627
SmirnovaMarya <unlenmaria@gmail.com>
2026-05-26 00:34:53 +0300

    add interface bstree

---
+++ smirnova.marya/common/bstree.hpp
@@ -0,0 +1,181 @@
+#ifndef BSTREE_HPP
+#define BSTREE_HPP
+
+#include <cstddef>
+#include <functional>
+#include <stdexcept>
+#include <utility>
+
+namespace smirnova
+{
+  struct TreeNodeBase
+  {
+    TreeNodeBase* parent_;
+    TreeNodeBase* left_;
+    TreeNodeBase* right_;
+
+    TreeNodeBase():
+      parent_(nullptr),
+      left_(nullptr),
+      right_(nullptr)
+    {}
+  };
+
+  template< class Key, class Value >
+  struct TreeNode: TreeNodeBase
+  {
+    std::pair< Key, Value > data_;
+
+    TreeNode():
+      TreeNodeBase(),
+      data_()
+    {}
+
+    TreeNode(const Key& key, const Value& value, TreeNodeBase* parent = nullptr):
+      TreeNodeBase(),
+      data_(key, value)
+    {
+      parent_ = parent;
+      left_ = nullptr;
+      right_ = nullptr;
+    }
+  };
+
+  template< class Key, class Value >
+  using Node = TreeNode< Key, Value >;
+}
+
+#include "tree-iter.hpp"
+
+namespace smirnova
+{
+  template< class NodeT >
+  TreeNodeBase* makeFakeLeaf();
+
+  TreeNodeBase* getMin(TreeNodeBase* node, TreeNodeBase* fake);
+
+  template< class Key, class Value >
+  class BSTree
+  {
+  public:
+    using NodeT = Node< Key, Value >;
+
+    using key_type = Key;
+
+    using iterator = BSTIterator< Key, Value >;
+    using const_iterator = BSTConstIterator< Key, Value >;
+
+    BSTree():
+      root_(nullptr),
+      fake_leaf_(makeFakeLeaf< NodeT >()),
+      size_(0)
+    {}
+
+    BSTree(const BSTree& other):
+      root_(nullptr),
+      fake_leaf_(makeFakeLeaf< NodeT >()),
+      size_(other.size_)
+    {
+      root_ = copy(other.root_, fake_leaf_, other.fake_leaf_);
+    }
+
+    BSTree(BSTree&& other) noexcept:
+      root_(other.root_),
+      fake_leaf_(other.fake_leaf_),
+      size_(other.size_)
+    {
+      other.root_ = nullptr;
+      other.fake_leaf_ = makeFakeLeaf< NodeT >();
+      other.size_ = 0;
+    }
+
+    BSTree& operator=(BSTree other)
+    {
+      swap(other);
+      return *this;
+    }
+
+    ~BSTree()
+    {
+      clear(root_);
+      delete static_cast< NodeT* >(fake_leaf_);
+    }
+
+    void swap(BSTree& other) noexcept;
+
+    bool empty() const noexcept
+    {
+      return size_ == 0;
+    }
+
+    size_t size() const noexcept
+    {
+      return size_;
+    }
+
+    iterator begin() noexcept;
+
+    iterator end() noexcept;
+
+    const_iterator begin() const noexcept;
+
+    const_iterator end() const noexcept;
+
+    const_iterator cbegin() const noexcept;
+
+    const_iterator cend() const noexcept;
+
+    bool contains(const Key& key) const noexcept;
+
+    void push(const Key& key, const Value& value);
+
+    Value& get(const Key& key);
+
+    const Value& get(const Key& key) const;
+
+    Value drop(const Key& key);
+
+    size_t height() const;
+
+    size_t height(const_iterator it) const;
+
+    iterator rotateLeft(iterator it);
+
+    iterator rotateRight(iterator it);
+
+    iterator rotateLargeLeft(iterator it);
+
+    iterator rotateLargeRight(iterator it);
+
+  private:
+    TreeNodeBase* root_;
+    TreeNodeBase* fake_leaf_;
+    size_t size_;
+
+    void clear(TreeNodeBase* node) noexcept;
+
+    TreeNodeBase* copy(
+      TreeNodeBase* other,
+      TreeNodeBase* parent,
+      TreeNodeBase* otherFake
+    );
+
+    NodeT* findNode(const Key& key) const noexcept;
+
+    size_t height(TreeNodeBase* node) const noexcept;
+
+    void transplant(TreeNodeBase* u, TreeNodeBase* v);
+
+    TreeNodeBase* rotateLeftNode(TreeNodeBase* x);
+
+    TreeNodeBase* rotateRightNode(TreeNodeBase* x);
+
+    TreeNodeBase* rotateLargeLeftNode(TreeNodeBase* x);
+
+    TreeNodeBase* rotateLargeRightNode(TreeNodeBase* x);
+  };
+}
+
+#include "bstree_impl.hpp"
+
+#endif
