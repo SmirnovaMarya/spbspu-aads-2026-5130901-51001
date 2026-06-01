5d5c14a3e5e03ae472a73646d457700ccb5586b7
SmirnovaMarya <unlenmaria@gmail.com>
2026-06-01 22:46:45 +0300

    fix CG

--- smirnova.marya/common/bstree.hpp
+++ smirnova.marya/common/bstree.hpp
@@ -20,26 +20,7 @@ namespace smirnova
     }
   };
 
-  template< class Key, class Value >
-  struct TreeNode: TreeNodeBase
-  {
-    std::pair< Key, Value > data_;
-
-    TreeNode():
-      TreeNodeBase(),
-      data_()
-    {}
-
-    TreeNode(const Key& key, const Value& value, TreeNodeBase* parent = nullptr):
-      TreeNodeBase(),
-      data_(key, value)
-    {
-      parent_ = parent;
-      left_ = nullptr;
-      right_ = nullptr;
-    }
-  };
-
+  // `TreeNode` is defined in treeNode.hpp; use that definition here.
   template< class Key, class Value >
   using Node = TreeNode< Key, Value >;
 }
