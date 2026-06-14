8a34749773295cfe5d3fceff422406322fbc93a9
SmirnovaMarya <unlenmaria@gmail.com>
2026-06-14 03:25:03 +0300

    Added methods for insertion, deletion, and the load factor of the cuckoo hash table

--- smirnova.marya/F0/CuckooHashTable.hpp
+++ smirnova.marya/F0/CuckooHashTable.hpp
@@ -51,8 +51,68 @@ namespace smirnova
     {
       return get(key) != nullptr;
     }
+    double loadFactor() const
+    {
+      std::size_t used = 0;
+
+      for (std::size_t i = 0; i < capacity_; ++i)
+      {
+        if (table1_[i].used)
+        {
+          ++used;
+        }
+        if (table2_[i].used)
+        {
+          ++used;
+        }
+      }
+      return (double)used / (capacity_ * 2);
+    }
+    bool erase(const Key& key)
+    {
+      std::size_t i1 = hash_.h1(key) % capacity_;
+
+      if (table1_[i1].used && table1_[i1].key == key)
+      {
+        table1_[i1].deleted = true;
+        return true;
+      }
+
+      std::size_t i2 = hash_.h2(key) % capacity_;
+
+      if (table2_[i2].used && table2_[i2].key == key)
+      {
+        table2_[i2].deleted = true;
+        return true;
+      }
+      return false;
+    }
+    bool insert(const Key& key, const Value& value)
+    {
+      if (loadFactor() > 0.7)
+      {
+        rehash(capacity_ * 2);
+      }
+
+      std::size_t i1 = hash_.h1(key) % capacity_;
+
+      if (!table1_[i1].used)
+      {
+        table1_[i1] = {key, value, true, false};
+        return true;
+      }
+
+      std::size_t i2 = hash_.h2(key) % capacity_;
+
+      if (!table2_[i2].used)
+      {
+        table2_[i2] = {key, value, true, false};
+        return true;
+      }
+      return false;
+    }
   private:
-    void allocate(std::size_t n)
+    void allocate(size_t n)
     {
       table1_.clear();
       table2_.clear();
